931132297d36bc185d2a65a61feb806698e20146
Hwang Juhee <julia0276@naver.com>
2022-08-31 19:04:25 +0900

    :sparkles: solve [11021, 11022번] 반복문 : Template Literals 활용한 A+B 출력

---
+++ BOJ/3_반복문/11022.js
@@ -0,0 +1,8 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let caseNum = parseInt(input[0]);
+
+for (let i=1; i<=caseNum ; i++) {
+  let numbers = input[i].toString().split(' ').map(Number);
+  console.log(`Case #${i}: ${numbers[0]} + ${numbers[1]} = ${numbers[0]+numbers[1]}`);
+}

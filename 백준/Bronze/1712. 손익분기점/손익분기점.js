38841a9cc3c70d594b3358065ef56b446411e578
Juhee Hwang <33420714+Juhee-Hwang@users.noreply.github.com>
2022-09-05 17:55:03 +0900

    Delete 백준/Bronze/1712. 손익분기점 directory

--- 백준/Bronze/1712. 손익분기점/손익분기점.js
+++
@@ -1,9 +0,0 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
-
-const a = input[0]; 
-const b = input[1]; 
-const c = input[2]; 
-const margin = c-b;
-
-const count = Math.floor(a/margin) +1
-console.log(margin <= 0 ? -1 : count);

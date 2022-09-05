0db61ba66f10808f8e28ee3dc6c4ad6585bfac9a
Juhee Hwang <33420714+Juhee-Hwang@users.noreply.github.com>
2022-09-05 17:36:23 +0900

    [Bronze II] Title: 손익분기점, Time: 116 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split(' ').map(Number);
+
+const a = input[0]; 
+const b = input[1]; 
+const c = input[2]; 
+const margin = c-b;
+
+const count = Math.floor(a/margin) +1
+console.log(margin <= 0 ? -1 : count);

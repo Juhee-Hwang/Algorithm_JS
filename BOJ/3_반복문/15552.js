bf6abac85c66f860d5182879cd5f915af7feda20
Hwang Juhee <julia0276@naver.com>
2022-09-06 23:31:59 +0900

    :sparkles: solve [15552번] 반복문 : A+B 시간제한

--- BOJ/3_반복문/15552.js
+++ BOJ/3_반복문/15552.js
@@ -1,8 +1,11 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-let test = input[0];
+let test = Number(input[0]);
+let result = "";
 
 for (let i=1; i<=test; i++){
-  let numbers = input[i].split(' ').map(Number);
-  console.log(numbers[0]+numbers[1]);
+  let numbers = input[i].split(' ');
+  result += Number(numbers[0])+Number(numbers[1]) + "\n";
 }
+
+console.log(result);

ac8856dd1fee314c047c7dbfa8a7d78fd88ce6a7
Juhee-Hwang <33420714+Juhee-Hwang@users.noreply.github.com>
2022-10-21 22:29:34 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 136 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -0,0 +1,8 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+let answer = new Array(26).fill(0);
+for (let i = 0; i<input.length; i++){
+  let alphabetNum = input.charCodeAt(i);
+  answer[alphabetNum-97]++ ;
+}
+
+console.log(answer.join(' '));

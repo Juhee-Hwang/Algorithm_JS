cb5a9fcffae38d79ae863f2c9dad21c4c43a6e1e
황주희 <33420714+Juhee-Hwang@users.noreply.github.com>
2023-01-12 16:58:13 +0900

    [Bronze I] Title: 소인수분해, Time: 252 ms, Memory: 9880 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.js
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -1,12 +1,17 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim();
-let n = Number(input);
+let n = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
 let i = 2;
+let answer = [];
 
-while (n != 1) {
-  if (n%i == 0){
-    console.log(i);
-    n = n/i;
-  } else {
-    i++;
+while (true) {
+  if (n % i == 0){
+      n = n / i;
+      answer.push(i);
+      i = 1;
+  }
+  i++; 
+  if (i > n) {
+    break;
   }
 }
+
+console.log(answer.join('\n'));

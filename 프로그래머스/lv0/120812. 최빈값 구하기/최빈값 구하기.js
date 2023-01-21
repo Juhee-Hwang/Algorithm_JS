278a2cf557c26f50d90468d2ec39486ce7c9bd8e
황주희 <33420714+Juhee-Hwang@users.noreply.github.com>
2023-01-22 01:48:11 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.45 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,6 +1,6 @@
 function solution(array) {
     let map = new Map();
-    
+
     // Map 객체 초기화
     for(let i = 0; i <= Math.max(...array); i++){
         map.set(i, 0);    
@@ -11,12 +11,16 @@ function solution(array) {
         map.set(array[i], map.get(array[i]) + 1);
     }
     
-    // Map 객체의 value만 모아서 배열로 만든다
-    let arr = Array.from(map.values());
+    let arr = [];
+    map.forEach((n) => arr.push(n));
     
+    
+    // Map 객체의 value만 모아서 배열로 만든다
+    // let arr = Array.from(map.values());
+
     // 최대값 산출
     let max = Math.max(...arr);
-    
+
     // 최대값이 하나만 있다면 max, 여러 개 중복된다면 -1 출력
     if(arr.indexOf(max) !== arr.lastIndexOf(max)){
         return -1;

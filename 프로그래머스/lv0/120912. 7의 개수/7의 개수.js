56c667cb444ecaa5a1d7b9b1c4b1f4521d517a72
Juhee-Hwang <julia0276@naver.com>
2023-04-19 22:42:03 +0900

    [✨ solve] 7의 개수 풀이 추가

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,15 +1,18 @@
 const getElNum = (arr, el) => arr.reduce((ac, v) => ac + (v === el), 0);
 
 function solution(array) {
-    let answer = 0;
-    
-    array.forEach((num) => {
-        let x = num.toString().split('')
-        if (x.includes('7')) {
-            answer += getElNum(x,'7');
-        }
-    })
-    return answer;
+  let answer = 0;
+
+  array.forEach((num) => {
+    let x = num.toString().split("");
+    if (x.includes("7")) {
+      answer += getElNum(x, "7");
+    }
+  });
+  return answer;
 }
 
-
+// 다른 사람 풀이인데, 심플하고 좋아서 나도 이 생각해서 가져옴
+function solution(array) {
+  return array.join("").split("7").length - 1;
+}

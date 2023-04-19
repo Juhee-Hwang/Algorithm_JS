ef70f6034a2dcf789c43b16428b928a458538543
Juhee-Hwang <julia0276@naver.com>
2023-04-19 22:41:54 +0900

    [✨ solve]  17829추가

--- 백준/Silver/17829. 222－풀링/222－풀링.js
+++ 백준/Silver/17829. 222－풀링/222－풀링.js
@@ -8,18 +8,23 @@ input.forEach((x) => {
   array.push(x.split(" ").map(Number));
 });
 
+// size는 해당영역의 크기, x,y는 해당영역의 시작점
 function pooling(size, x, y) {
   let mid = (size / 2) | 0;
+  // 영역의 크기가 2라면
   if (size == 2) {
+    // 해당영역에 있는 4개의 값을 배열에 저장하고
     let answer = [
       array[x][y],
       array[x + 1][y],
       array[x][y + 1],
       array[x + 1][y + 1],
     ];
+    // 내림차순 정렬 후 2번째 값을 반환
     answer.sort((a, b) => b - a);
     return answer[1];
   }
+  // 그렇지 않은 경우 영역을 4개의 작은 영역으로 분할한 후, 각 영역의 2번째 값을 배열에 저장
   let lt = pooling(mid, x, y);
   let rt = pooling(mid, x + mid, y);
   let lb = pooling(mid, x, y + mid);
